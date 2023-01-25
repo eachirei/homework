7333408e179d1ebfab5ff0e572d31a057bcecf0c
Emanuel ACHIREI <emanuel.achirei@gmail.com>
2023-01-25 22:41:05 +0200

    feat(balance): implement deposit

--- src/app.js
+++ src/app.js
@@ -151,6 +151,83 @@ router.post('/jobs/:job_id/pay', async (req, res, next) => {
     return res.status(200).end();
 });
 
+const canUserDeposit = async (profile, toDeposit, transaction) => {
+    const query = {
+        where: {
+            ...getQueryByProfileType(profile),
+            ...getQueryByState('active'),
+            '$Jobs.paid$': { [Op.or]: [null, false] },
+        },
+    };
+
+    const { Job } = app.get('models');
+
+    const { Contract } = app.get('models');
+
+    const dbResult = await Contract.findAll(
+        {
+            ...query,
+            include: [
+                {
+                    model: Job,
+                    attributes: [],
+                },
+            ],
+            attributes: [[sequelize.fn('SUM', sequelize.literal('Jobs.price')), 'jobsPrice']],
+        },
+        { transaction }
+    );
+
+    const remainingToPay = dbResult.length ? +dbResult[0].getDataValue('jobsPrice') : 0;
+
+    const maxToDeposit = remainingToPay > 0 ? 0.25 * remainingToPay : Number.POSITIVE_INFINITY;
+
+    return toDeposit < maxToDeposit;
+};
+
+router.post('/balances/deposit/:user_id', async (req, res, next) => {
+    const { Profile } = req.app.get('models');
+
+    const toDeposit = req.body.deposit;
+    const sourceProfile = req.profile;
+    const destinationProfileId = parseInt(req.params.user_id);
+
+    if (sourceProfile.id === destinationProfileId) {
+        return res.status(400).end();
+    }
+
+    try {
+        await sequelize.transaction(async t => {
+            const destinationProfile = await Profile.findOne({ id: destinationProfileId });
+            if (!destinationProfile) {
+                throw new Error('404');
+            }
+
+            if (sourceProfile.balance < toDeposit) {
+                throw new Error('403');
+            }
+            if (sourceProfile.type === 'client') {
+                // query included here to make sure no db changes happen between the job query and the deposit
+                const canDeposit = await canUserDeposit(sourceProfile, toDeposit, t);
+                if (!canDeposit) {
+                    throw new Error('403');
+                }
+            }
+
+            await sourceProfile.update({ balance: sourceProfile.balance - toDeposit }, { transaction: t });
+            await destinationProfile.update({ balance: sourceProfile.balance + toDeposit }, { transaction: t });
+        });
+    } catch (error) {
+        const possibleErrorCode = parseInt(error.message, 10);
+        if (!Number.isNaN(possibleErrorCode)) {
+            return res.status(possibleErrorCode).end();
+        }
+        return res.status(500).end();
+    }
+
+    return res.status(200).end();
+});
+
 app.use(router);
 
 module.exports = app;
