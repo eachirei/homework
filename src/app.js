5effa47bca1bccdb15e1248c341641a43fb6dcd4
Emanuel ACHIREI <emanuel.achirei@gmail.com>
2023-01-25 21:58:39 +0200

    fix(jobs/payment): add job find to transaction; use transaction properly

--- src/app.js
+++ src/app.js
@@ -109,34 +109,42 @@ router.post('/jobs/:job_id/pay', async (req, res, next) => {
 
     const { job_id } = req.params;
 
-    const job = await Job.findOne({
-        where: {
-            id: job_id,
-            paid: { [Op.or]: [null, false] },
-            '$Contract.ClientId$': req.profile.id,
-        },
-        include: [{ model: Contract, include: [{ model: Profile, as: 'Contractor' }] }],
-    });
-
-    if (!job) {
-        return res.status(404).end();
-    }
-
-    const contractorProfile = job.Contract.Contractor;
-
-    const clientProfile = req.profile;
-
-    if (job.price > clientProfile.balance) {
-        return res.status(403).end();
-    }
-
     try {
         await sequelize.transaction(async t => {
-            await clientProfile.update({ balance: clientProfile.balance - job.price });
-            await contractorProfile.update({ balance: clientProfile.balance + job.price });
-            await job.update({ paid: true, paymentDate: new Date() });
+            // find the job in the transaction to ensure that between finding the job and actual payment the job isn't paid twice
+            const job = await Job.findOne(
+                {
+                    where: {
+                        id: job_id,
+                        paid: { [Op.or]: [null, false] },
+                        '$Contract.ClientId$': req.profile.id,
+                    },
+                    include: [{ model: Contract, include: [{ model: Profile, as: 'Contractor' }] }],
+                },
+                { transaction: t }
+            );
+
+            if (!job) {
+                throw new Error({ message: '404' });
+            }
+
+            const contractorProfile = job.Contract.Contractor;
+
+            const clientProfile = req.profile;
+
+            if (job.price > clientProfile.balance) {
+                throw new Error({ message: '403' });
+            }
+
+            await clientProfile.update({ balance: clientProfile.balance - job.price }, { transaction: t });
+            await contractorProfile.update({ balance: clientProfile.balance + job.price }, { transaction: t });
+            await job.update({ paid: true, paymentDate: new Date() }, { transaction: t });
         });
     } catch (error) {
+        const possibleErrorCode = parseInt(error.message, 10);
+        if (!Number.isNaN(possibleErrorCode)) {
+            return res.status(possibleErrorCode).end();
+        }
         return res.status(500).end();
     }
 
