87428a228e01f024c1b9f16d874a319db31bbf14
Emanuel ACHIREI <emanuel.achirei@gmail.com>
2023-01-25 19:38:19 +0200

    fix(contracts): query contract by foreign key

--- src/app.js
+++ src/app.js
@@ -8,14 +8,26 @@ app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
 /**
- * FIX ME!
  * @returns contract by id
  */
 app.get('/contracts/:id', getProfile, async (req, res) => {
     const { Contract } = req.app.get('models');
     const { id } = req.params;
-    const contract = await Contract.findOne({ where: { id } });
-    if (!contract) return res.status(404).end();
-    res.json(contract);
+    const {type, id: profileId} = req.profile;
+
+    // query by contractor id or client id based on type of profile
+    const query = {
+        where: {
+            id,
+            ...(type === 'contractor' ? {'ContractorId': profileId} : {'ClientId': profileId})
+        },
+    };
+
+    const contract = await Contract.findOne(query);
+    if (!contract) {
+        return res.status(404).end();
+    }
+
+    return res.json(contract);
 });
 module.exports = app;
