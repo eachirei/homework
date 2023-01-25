04dee0d2fb686616b8bb6816491f5556dfe2e963
Emanuel ACHIREI <emanuel.achirei@gmail.com>
2023-01-25 19:43:44 +0200

    refactor: use router to reuse getProfile

--- src/app.js
+++ src/app.js
@@ -2,24 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { sequelize } = require('./model');
 const { getProfile } = require('./middleware/getProfile');
+const router = express.Router();
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
+router.use(getProfile);
+
 /**
  * @returns contract by id
  */
-app.get('/contracts/:id', getProfile, async (req, res) => {
+router.get('/contracts/:id', async (req, res) => {
     const { Contract } = req.app.get('models');
     const { id } = req.params;
-    const {type, id: profileId} = req.profile;
+    const { type, id: profileId } = req.profile;
 
     // query by contractor id or client id based on type of profile
     const query = {
         where: {
             id,
-            ...(type === 'contractor' ? {'ContractorId': profileId} : {'ClientId': profileId})
+            ...(type === 'contractor' ? { ContractorId: profileId } : { ClientId: profileId }),
         },
     };
 
@@ -30,4 +33,7 @@ app.get('/contracts/:id', getProfile, async (req, res) => {
 
     return res.json(contract);
 });
+
+app.use(router);
+
 module.exports = app;
