4fe1854c30b4ca8877950aa73bb330335cc2675f
Emanuel ACHIREI <emanuel.achirei@gmail.com>
2023-01-25 22:53:17 +0200

    refactor(jobs): move to separate file

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { Op } = require('sequelize');
 const router = express.Router();
 const app = express();
 const contractsRouter = require('./routes/contracts');
+const jobsRouter = require('./routes/jobs');
 const { getQueryByProfileType,
     getQueryByState
 } = require('./utils');
@@ -14,80 +15,11 @@ app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
 app.use(contractsRouter);
+app.use(jobsRouter);
 
 router.use(getProfile);
 
-router.get('/jobs/unpaid', async (req, res, next) => {
-    const { Job } = req.app.get('models');
 
-    const { Contract } = app.get('models');
-
-    const query = {
-        where: {
-            ...getQueryByProfileType(req.profile),
-            ...getQueryByState('active'),
-            '$Jobs.paid$': { [Op.or]: [null, false] },
-        },
-    };
-
-    const contractsWithJobs = await Contract.findAll({
-        ...query,
-        include: [
-            {
-                model: Job,
-            },
-        ],
-    });
-
-    return res.json(contractsWithJobs.map(c => c.Jobs).flat());
-});
-
-router.post('/jobs/:job_id/pay', async (req, res, next) => {
-    const { Job, Contract, Profile } = req.app.get('models');
-
-    const { job_id } = req.params;
-
-    try {
-        await sequelize.transaction(async t => {
-            // find the job in the transaction to ensure that between finding the job and actual payment the job isn't paid twice
-            const job = await Job.findOne(
-                {
-                    where: {
-                        id: job_id,
-                        paid: { [Op.or]: [null, false] },
-                        '$Contract.ClientId$': req.profile.id,
-                    },
-                    include: [{ model: Contract, include: [{ model: Profile, as: 'Contractor' }] }],
-                },
-                { transaction: t }
-            );
-
-            if (!job) {
-                throw new Error('404');
-            }
-
-            const contractorProfile = job.Contract.Contractor;
-
-            const clientProfile = req.profile;
-
-            if (job.price > clientProfile.balance) {
-                throw new Error('403');
-            }
-
-            await clientProfile.update({ balance: clientProfile.balance - job.price }, { transaction: t });
-            await contractorProfile.update({ balance: clientProfile.balance + job.price }, { transaction: t });
-            await job.update({ paid: true, paymentDate: new Date() }, { transaction: t });
-        });
-    } catch (error) {
-        const possibleErrorCode = parseInt(error.message, 10);
-        if (!Number.isNaN(possibleErrorCode)) {
-            return res.status(possibleErrorCode).end();
-        }
-        return res.status(500).end();
-    }
-
-    return res.status(200).end();
-});
 
 const canUserDeposit = async (profile, toDeposit, transaction) => {
     const query = {
