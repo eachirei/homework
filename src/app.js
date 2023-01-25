0675b1cd072460ee05ffe275a3ff1df72948ecfa
Emanuel ACHIREI <emanuel.achirei@gmail.com>
2023-01-25 19:55:55 +0200

    feat(contracts): add contracts list

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { sequelize } = require('./model');
 const { getProfile } = require('./middleware/getProfile');
+const { Op } = require('sequelize');
 const router = express.Router();
 const app = express();
 app.use(bodyParser.json());
@@ -10,19 +11,19 @@ app.set('models', sequelize.models);
 
 router.use(getProfile);
 
+const getQueryByProfileType = ({ type, id }) => (type === 'contractor' ? { ContractorId: id } : { ClientId: id });
+
 /**
  * @returns contract by id
  */
 router.get('/contracts/:id', async (req, res) => {
     const { Contract } = req.app.get('models');
     const { id } = req.params;
-    const { type, id: profileId } = req.profile;
 
-    // query by contractor id or client id based on type of profile
     const query = {
         where: {
+            ...getQueryByProfileType(req.profile),
             id,
-            ...(type === 'contractor' ? { ContractorId: profileId } : { ClientId: profileId }),
         },
     };
 
@@ -32,6 +33,25 @@ router.get('/contracts/:id', async (req, res) => {
     }
 
     return res.json(contract);
+});
+
+router.get('/contracts', async (req, res, next) => {
+    const { Contract } = req.app.get('models');
+
+    // query by contractor id or client id based on type of profile
+    const query = {
+        where: {
+            ...getQueryByProfileType(req.profile),
+            [Op.not]: { status: 'terminated' },
+        },
+    };
+
+    const contracts = await Contract.findAll(query);
+    if (!contracts) {
+        return res.status(404).end();
+    }
+
+    return res.json(contracts);
 });
 
 app.use(router);
